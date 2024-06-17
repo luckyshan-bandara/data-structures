e13ae7f3d3decf40e26654e2967a2c947adcf749
luckyshanb <lbandara@infomedia.com.au>
2024-06-17 11:32:32 +0530

    feat: explanation how BST visually

--- src/com/company/binarysearchtree/Main.java
+++ src/com/company/binarysearchtree/Main.java
@@ -3,15 +3,14 @@ package com.company.binarysearchtree;
 public class Main {
     public static void main(String[] args) {
         Tree bst = new Tree();
+        // Note: Here we try to inout same numbers but in different sequence to see how BST is build each time.
+//        int[] inputArray = {66, 12, 100, 25, 35, 15, 5, 10}; // bst-1.png
+//        int[] inputArray = {5, 10, 15, 66, 12, 100, 25, 35}; // bst-2.png
+        int[] inputArray = {25, 12, 66, 10, 15, 35, 100, 5}; // bst-3.png
 
-        bst.insert(66);
-        bst.insert(12);
-        bst.insert(100);
-        bst.insert(25);
-        bst.insert(35);
-        bst.insert(15);
-        bst.insert(5);
-        bst.insert(10);
+        for (int i=0; i< inputArray.length; i++) {
+            bst.insert(inputArray[i]);
+        }
 
         bst.traverseInOrder();
     }
